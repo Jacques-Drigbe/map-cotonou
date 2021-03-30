0c392db159f0894fae0a71d5192f2e56018c0716
Jacques-Drigbe <73288308+Jacques-Drigbe@users.noreply.github.com>
2021-03-31 00:07:46 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,1 @@
-fetch('http://vtr21ben.pac.bj/WS_ESCALES?test#/WS_Escales|GET')
-  .then(response => response.json())
-  .then(commits => alert(commits[0].author.login));
+fetch('https://vtr21ben.pac.bj/WS_ESCALES?test#/WS_Escales|GET');
